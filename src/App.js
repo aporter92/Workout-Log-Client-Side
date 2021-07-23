e4c382cf7af48d83a561c427d89d4f9070735187
Anne Porter <a.porter9212@gmail.com>
2021-07-23 15:45:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import SiteBar from './home/Navbar';
+import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [sessionToken, setSessionToken] = useState(''); 
 
+useEffect(() => { 
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+}, [])
+
+const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(newToken);
+} 
+const clearToken = () => {
+  localStorage.clear();
+  setSessionToken('');
+}
+const protectedViews = () => {
+  return (sessionToken === localStorage.getItem('token') 
+  ? <WorkoutIndex token ={sessionToken} /> : <Auth updateToken = {updateToken}/>)
+}
+return (
+    <div>
+      <SiteBar clearToken = {clearToken}/>
+      {protectedViews()}
+    </div>
+); 
+}
 export default App;
